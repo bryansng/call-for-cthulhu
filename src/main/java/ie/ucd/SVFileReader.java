2aee759781e48188e062cd9ca53577fa0584ddb0
ankish <ankish12raj@gmail.com>
2020-03-25 16:59:50 +0000

    code to write projects to CSV, untested

---
+++ src/main/java/ie/ucd/SVFileReader.java
@@ -0,0 +1,11 @@
+package ie.ucd;
+
+import org.supercsv.cellprocessor.*;
+import org.supercsv.io.CsvBeanReader;
+import org.supercsv.io.ICsvBeanReader;
+import org.supercsv.prefs.CsvPreference;
+
+public class SVFileReader {
+    public SVFileReader() {
+    }
+}
