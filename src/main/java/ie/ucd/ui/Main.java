13d420b004015dc6352ade3ba616dd83617112f5
Ronan Mascarenhas <ronansm2@gmail.com>
2020-04-14 19:40:38 +0100

    rough setup ui done

--- src/main/java/ie/ucd/ui/Main.java
+++ src/main/java/ie/ucd/ui/Main.java
@@ -1,5 +1,7 @@
 package ie.ucd.ui;
 
+import java.io.File;
+
 //import java.awt.Checkbox;
 
 import javafx.application.Application;
@@ -12,12 +14,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class Main extends Application //implements EventHandler
@@ -51,7 +55,7 @@ public class Main extends Application //implements EventHandler
 		GridPane grid = new GridPane();
 		grid.setPadding(new Insets(10, 10, 10, 10));
 		
-		//hard/soft constraints create
+		//hard/soft/other constraints create
 		VBox vboxHardConstraints = new VBox();
 		
 		CheckBox hardOne = new CheckBox("Each student is assigned exactly one of their preferred projects");
@@ -60,11 +64,14 @@ public class Main extends Application //implements EventHandler
 		CheckBox softOne = new CheckBox("higher GPA means a greater chance of getting one's preferred project");
 		CheckBox softTwo = new CheckBox("projects are distributed roughly equally between supervisors");
 		Text headingOne = new Text("1. Settings");
-
-		//TilePane tilePane = new TilePane();
-		//tilePane.getChildren().add(hardOne);
 		
-		vboxHardConstraints.getChildren().addAll(headingOne, hardOne, hardTwo, hardThree, softOne, softTwo);
+		Slider gpaSlider = new Slider(0, 1, 0);
+		gpaSlider.setShowTickMarks(true);
+		gpaSlider.setShowTickLabels(true);
+		gpaSlider.setMajorTickUnit(0.25f);
+		
+		vboxHardConstraints.getChildren().addAll(headingOne, hardOne, hardTwo, 
+		hardThree, softOne, softTwo, gpaSlider);
 		grid.setConstraints(vboxHardConstraints, 0, 0);
 		grid.setConstraints(hardOne, 1, 0);
 		grid.setConstraints(hardOne, 2, 0);
@@ -73,13 +80,52 @@ public class Main extends Application //implements EventHandler
 		grid.setConstraints(softOne, 5, 0);
 		grid.getChildren().addAll(vboxHardConstraints);
 		scrollPane.setContent(grid);
-		scrollPane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+		//scrollPane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 		borderPane.setCenter(scrollPane);
+		
+		//project section create
+		Text headingTwo = new Text("2. Load/Generate Projects");
+		FileChooser fileChooserOne = new FileChooser();
+		Button browseProjectButton= new Button("Browse for Project File");
+		browseProjectButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                File file = fileChooserOne.showOpenDialog(primaryStage);
+                if (file != null) {
+                    //openFile(file);		if file is valid, display name in field
+                }
+            }
+        });
+		Button generateRandomProject = new Button("Generate Random Project");
+		vboxHardConstraints.getChildren().addAll(headingTwo, browseProjectButton, generateRandomProject);
+		
+		
+		//student section create
+		Text headingThree= new Text("3. Load/Generate Students");
+		FileChooser fileChooserTwo = new FileChooser();
+		Button browseStudentButton= new Button("Browse for Student File");
+		browseStudentButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                File file = fileChooserTwo.showOpenDialog(primaryStage);
+                if (file != null) {
+                    //openFile(file);		if file is valid, display name in field
+                }
+            }
+        });
+		Button generateRandomStudent= new Button("Generate Random Project");
+		vboxHardConstraints.getChildren().addAll(headingThree, browseStudentButton, generateRandomStudent);	
+		
+		
+		
+		
+		 
+		 //gpaSlider.setBlockIncrement(0.1f);
 		
 		//vboxHardConstraints.getChildren().addAll()
 		
 	
-		//constraints create
+		
 		Scene setupScene = new Scene(borderPane, 600, 400);
 		//root.getChildren().add(button);
 		primaryStage.setScene(setupScene);
